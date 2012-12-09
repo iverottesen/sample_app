2231fc0d88f7b6e6644627cf114b20d4c6b286b2
Iver Ottesen <iver.ottesen@gmail.com>
2012-12-09 19:21:29 +0100

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a pre-page basis.
+  def full_title(page_title)
+    base_title = 'Iver Ottesen'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,15 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Sample App'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Iver Ottesen | Sample App')
+      page.should have_selector('title', :text => 'Iver Ottesen')
     end
+
+    it "should not have a costum page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end 
   end
 
 
